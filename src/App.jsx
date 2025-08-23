e6a99fa3484b467679b9dd6d18a990ca3d40fc5e
Hammad Ali <hammadali78056@gmail.com>
2025-08-23 12:38:43 +0500

    fix: adding appwrite credential

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ console.log(import.meta.env.REACT_APP_APPWRITE_URL)
 
   return (
     <h1 className="text-3xl font-bold underline text-blue-500">Blog App</h1>
+
   )
 }
 
