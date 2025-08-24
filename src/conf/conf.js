bb52084deee1e12c800743609f4f29ab8847b81c
Hammad Ali <hammadali78056@gmail.com>
2025-08-24 11:32:06 +0500

    feat : build authentication with appwite

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,6 +1,6 @@
 const conf = {
 appwriteUrl: String(import.meta.env.REACT_APP_APPWRITE_URL),
-appwriteProject: String(import.meta.env.REACT_APP_APPWRITE_PROJECT_ID),
+appwriteProjectId: String(import.meta.env.REACT_APP_APPWRITE_PROJECT_ID),
 appwriteDatabaseId: String(import.meta.env.REACT_APP_APPWRITE_DATABASE_ID),
 appwriteCollectionId: String(import.meta.env.REACT_APP_APPWRITE_COLLECTION_ID),
 appwriteBucketId: String(import.meta.env.REACT_APP_APPWRITE_BUCKET_ID),
