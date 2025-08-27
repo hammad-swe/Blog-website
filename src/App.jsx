5f950b8136566c52db3e348a2538dbb025b716f8
Hammad Ali <hammadali78056@gmail.com>
2025-08-27 15:37:17 +0500

    fix: add reducer in store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
+import react, { useState, useEffect  } from 'react'
+import { useDispatch } from 'react-redux';
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import {Header, Footer} from './components/index'
+import { Outlet } from 'react-router-dom';
+
+
+
 
 function App() {
- console.log(import.meta.env.REACT_APP_APPWRITE_URL)
+ const [Loading , setLoading] = useState(true);
+const dispatch = useDispatch();
 
-  return (
-    <h1 className="text-3xl font-bold underline text-blue-500">Blog App</h1>
+useEffect(() => {
+authService.getCurrentUser()
+.then((userData)=>{
+if(userData){
+dispatch(login(userData));
+}
+else{
+  dispatch(logout());
+}
+})
+.finally(() => {
+setLoading(false);
+});
+}, [])
 
-  )
+return !Loading ? (
+  <div className="min-h-screen flex flex-wrap content-between bg-gray-400 ">
+    <div className='w-full block'>
+      <Header/>
+      <main>
+       todo: <Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  </div>
+) : null;
 }
 
-export default App
+export default App;
