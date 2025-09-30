0ffe98f043f6cd1ccbcbc9f5b47857a47d412151
Hammad Ali <hammadali78056@gmail.com>
2025-09-30 10:16:41 +0500

    Feat: build pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,28 @@ import Footer from './Footer/Footer'
 import Container from './Container/Container'
 import Logo from '../components/Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import RTE from './RTE'
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Authlayout from './AuthLayout'
+
+
 
 export { 
     Header, 
     Footer, 
     Container,
+    RTE,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    SignUp,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
